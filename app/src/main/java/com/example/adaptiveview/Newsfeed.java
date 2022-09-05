a1f71eb5e50950e924e5046694076fb96e8213bf
pemtshewang <pemtshewang500@gmail.com>
2022-09-05 17:02:28 +0600

    Editing recycle views

---
+++ app/src/main/java/com/example/adaptiveview/Newsfeed.java
@@ -0,0 +1,26 @@
+package com.example.adaptiveview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class Newsfeed extends AppCompatActivity {
+ImageView img1,img2,img3,img4;
+RecyclerView rView;
+    @SuppressLint("ResourceType")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_newsfeed);
+        img1 = findViewById(R.drawable.news1);
+        img2 = findViewById(R.drawable.news2);
+        img3 = findViewById(R.drawable.news3);
+        img4 = findViewById(R.drawable.news4);
+        rView = findViewById(R.id.)
+
+
+    }
+}
